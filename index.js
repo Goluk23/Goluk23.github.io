ece090eaf30e7b7767e1c62186416f75148e595b
Goluk23 <lgo@hotmail.be>
2026-02-13 02:41:50 +0100

    Ajout des boutons de navigation. Ajout des sections et id à chaque section. Navigation en JavaScript.

---
+++ index.js
@@ -0,0 +1,72 @@
+const btnnav = document.querySelector("nav #btn-nav");
+const btnvss = document.querySelector("nav #btn-vss");
+const btnhtml = document.querySelector("nav #btn-html");
+const btncss = document.querySelector("nav #btn-css");
+const btnjs = document.querySelector("nav #btn-js");
+const btngit = document.querySelector("nav #btn-git");
+const sectionnav = document.querySelector("#section-nav");
+const sectionvss = document.querySelector("#section-vss");
+const sectionhtml = document.querySelector("#section-html");
+const sectioncss = document.querySelector("#section-css");
+const sectionjs = document.querySelector("#section-js");
+const sectiongit = document.querySelector("#section-git");
+
+btnnav.addEventListener("click", () => {
+  sectionnav.style.border = "5px solid var(--colorNAV)";
+  sectionnav.style.display = "";
+  sectionvss.style.display = "none";
+  sectionhtml.style.display = "none";
+  sectioncss.style.display = "none";
+  sectionjs.style.display = "none";
+  sectiongit.style.display = "none";
+});
+
+btnvss.addEventListener("click", () => {
+  sectionvss.style.border = "5px solid var(--colorVSS)";
+  sectionnav.style.display = "none";
+  sectionvss.style.display = "";
+  sectionhtml.style.display = "none";
+  sectioncss.style.display = "none";
+  sectionjs.style.display = "none";
+  sectiongit.style.display = "none";
+});
+
+btnhtml.addEventListener("click", () => {
+  sectionhtml.style.border = "5px solid var(--colorHTML)";
+  sectionnav.style.display = "none";
+  sectionvss.style.display = "none";
+  sectionhtml.style.display = "";
+  sectioncss.style.display = "none";
+  sectionjs.style.display = "none";
+  sectiongit.style.display = "none";
+});
+
+btncss.addEventListener("click", () => {
+  sectioncss.style.border = "5px solid var(--colorCSS)";
+  sectionnav.style.display = "none";
+  sectionvss.style.display = "none";
+  sectionhtml.style.display = "none";
+  sectioncss.style.display = "";
+  sectionjs.style.display = "none";
+  sectiongit.style.display = "none";
+});
+
+btnjs.addEventListener("click", () => {
+  sectionjs.style.border = "5px solid var(--colorJS)";
+  sectionnav.style.display = "none";
+  sectionvss.style.display = "none";
+  sectionhtml.style.display = "none";
+  sectioncss.style.display = "none";
+  sectionjs.style.display = "";
+  sectiongit.style.display = "none";
+});
+
+btngit.addEventListener("click", () => {
+  sectiongit.style.border = "5px solid var(--colorGIT)";
+  sectionnav.style.display = "none";
+  sectionvss.style.display = "none";
+  sectionhtml.style.display = "none";
+  sectioncss.style.display = "none";
+  sectionjs.style.display = "none";
+  sectiongit.style.display = "";
+});
